227953e8c40a9bc271ece289e0a8da8ce8000e21
Drozerah <drozerah@gmail.com>
2020-10-09 11:35:07 +0200

    feat: add Title component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title titleTextContent="Fcc Markdown Previewer Editor"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title
+  },
+  mounted() {
+    console.log('app is monted') // !DEBUG
   }
 }
 </script>
@@ -23,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="title">
+    <h1>{{ titleTextContent }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    titleTextContent: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 30px;
+}
+</style>
